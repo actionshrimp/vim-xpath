aa907490347d45631edb47c5796a82bc4fef4f1a
Dave Aitken <dave.aitken@gmail.com>
2013-02-03 19:00:06 +0000

    Added workaround for line numbers > 65535

--- python/tests/xpath_tests.py
+++ python/tests/xpath_tests.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, sys
 
 from helpers.file_helpers import read_sample_xml
 
@@ -8,29 +8,33 @@ class XPathTests(unittest.TestCase):
     def test_node_xpath(self):
         evaluated = xpath.evaluate("<Root/>", "//Root")
 
-        self.assertEqual("Root", evaluated["display"])
-        self.assertEqual(1, evaluated["line_number"])
+        self.assertEqual("Root", evaluated[0]["display_text"])
+        self.assertEqual(1, evaluated[0]["line_number"])
 
     def test_deeper_node_xpath(self):
         evaluated = xpath.evaluate("<Root><Tag/></Root>", "//Tag")
 
-        self.assertEqual("Tag", evaluated["display"])
-        self.assertEqual(1, evaluated["line_number"])
+        self.assertEqual("Tag", evaluated[0]["display_text"])
+        self.assertEqual(1, evaluated[0]["line_number"])
 
     def test_multiline_xml(self):
         xml = read_sample_xml("tests/samples/simple.xml")
         evaluated = xpath.evaluate(xml, "//Tag")
 
-        self.assertEqual("Tag", evaluated["display"])
-        self.assertEqual(3, evaluated["line_number"])
+        self.assertEqual("Tag", evaluated[0]["display_text"])
+        self.assertEqual(3, evaluated[0]["line_number"])
 
     def test_large_xml_line_number(self):
         #Reported that lxml cuts off sourceline at 65535 due to
         #libxml2 limitation https://bugs.launchpad.net/lxml/+bug/674775
 
         xml = read_sample_xml("tests/samples/very_large.xml")
-        evaluated = xpath.evaluate(xml, "//Last")
+        evaluated = xpath.evaluate(xml, "//Match")
 
-        self.assertEqual("Last", evaluated["display"])
-        self.assertEqual(70000, evaluated["line_number"])
-
+        self.assertEqual(3, evaluated[0]["line_number"])
+        self.assertEqual(65534, evaluated[1]["line_number"])
+        self.assertEqual(65535, evaluated[2]["line_number"])
+        self.assertEqual(70000, evaluated[3]["line_number"])
+        self.assertEqual(135532, evaluated[4]["line_number"])
+        self.assertEqual(135533, evaluated[5]["line_number"])
+        self.assertEqual(139998, evaluated[6]["line_number"])
--- python/vim_xml_tools/xpath.py
+++ python/vim_xml_tools/xpath.py
@@ -1,11 +1,39 @@
+from StringIO import StringIO
 from lxml import etree
 
+LIBXML2_MAX_LINE = 65534
+
 def evaluate(xml, xpath):
-
     tree = etree.fromstring(xml)
-    evaluated = tree.xpath(xpath)
+    tree_matches = tree.xpath(xpath)
 
+    matches = []
+    fold_required = False
+
+    for match in tree_matches:
+        if match.sourceline <= LIBXML2_MAX_LINE:
+            output_match = tree_match_to_output_match(match, 0)
+            matches.append(output_match)
+        else:
+            fold_required = True
+            break;
+
+    if fold_required:
+        xml_folded = xml.replace("\n", "", LIBXML2_MAX_LINE)
+
+        folded_matches = evaluate(xml_folded, xpath)
+
+        new_output_matches = folded_matches[len(matches):]
+
+        for output_match in new_output_matches:
+            output_match["line_number"] += LIBXML2_MAX_LINE
+            matches.append(output_match)
+
+    return matches
+
+def tree_match_to_output_match(match, fold_offset):
     out = dict()
-    out["line_number"] = evaluated[0].sourceline
-    out["display"] = evaluated[0].tag
+    out["line_number"] = match.sourceline + fold_offset
+    out["display_text"] = match.tag
+
     return out
