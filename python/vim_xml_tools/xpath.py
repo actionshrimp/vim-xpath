3f51aca0e4a9febfd0d4fa2e87737cf09e821311
Dave Aitken <dave.aitken@gmail.com>
2013-02-03 19:27:53 +0000

    Tidied up and added some comments

--- python/vim_xml_tools/xpath.py
+++ python/vim_xml_tools/xpath.py
@@ -1,39 +1,54 @@
-from StringIO import StringIO
 from lxml import etree
 
 LIBXML2_MAX_LINE = 65534
 
 def evaluate(xml, xpath):
+    """Evaluate an xpath against some xml. 
+    Reports line numbers correctly on xml with over 65534 lines"""
+
     tree = etree.fromstring(xml)
     tree_matches = tree.xpath(xpath)
 
     matches = []
-    fold_required = False
+    line_compress_required = False
 
     for match in tree_matches:
         if match.sourceline <= LIBXML2_MAX_LINE:
-            output_match = tree_match_to_output_match(match, 0)
+            #Only keep matches which are in the line range that libxml2
+            #actually reports correctly
+            output_match = tree_match_to_output_match(match)
             matches.append(output_match)
         else:
-            fold_required = True
+            #If there are any higher range matches, we will have to 'fold'
+            line_compress_required = True
             break;
 
-    if fold_required:
-        xml_folded = xml.replace("\n", "", LIBXML2_MAX_LINE)
+    if line_compress_required:
+        #Compress the inital line range 1-65534 into a single line,
+        #to give us correct line numbers on the next 65534 lines.
+        line_compressed_xml = xml.replace("\n", "", LIBXML2_MAX_LINE)
 
-        folded_matches = evaluate(xml_folded, xpath)
+        #Re-evaluate with the new source XML recursively,
+        #to handle all higher line ranges
+        line_compressed_matches = evaluate(line_compressed_xml, xpath)
 
-        new_output_matches = folded_matches[len(matches):]
+        #Only take matches which we haven't already found, as the higher range
+        #evaluations will have incorrect line numbers for matches in lower ranges
+        #(the sourceline will be 1 for all lower range matches)
+        new_output_matches = line_compressed_matches[len(matches):]
 
         for output_match in new_output_matches:
+            #As we've compressed a bunch of lines down to 1 line, we need to
+            #offset the line number of the results to compensate for this
             output_match["line_number"] += LIBXML2_MAX_LINE
             matches.append(output_match)
 
     return matches
 
-def tree_match_to_output_match(match, fold_offset):
+def tree_match_to_output_match(match):
+    """Convert an lxml xpath match to a result output dictionary"""
     out = dict()
-    out["line_number"] = match.sourceline + fold_offset
+    out["line_number"] = match.sourceline
     out["display_text"] = match.tag
 
     return out
