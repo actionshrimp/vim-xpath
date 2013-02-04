70c94df478f7f55c763e053a2b2038942096e538
Dave Aitken <dave.aitken@gmail.com>
2013-02-04 00:18:25 +0000

    Namespace handling, and some handling of different output types

--- python/tests/xpath_tests.py
+++ python/tests/xpath_tests.py
@@ -8,20 +8,20 @@ class XPathTests(unittest.TestCase):
     def test_node_xpath(self):
         evaluated = xpath.evaluate("<Root/>", "//Root")
 
-        self.assertEqual("Root", evaluated[0]["display_text"])
+        self.assertEqual("Root", evaluated[0]["match_text"])
         self.assertEqual(1, evaluated[0]["line_number"])
 
     def test_deeper_node_xpath(self):
         evaluated = xpath.evaluate("<Root><Tag/></Root>", "//Tag")
 
-        self.assertEqual("Tag", evaluated[0]["display_text"])
+        self.assertEqual("Tag", evaluated[0]["match_text"])
         self.assertEqual(1, evaluated[0]["line_number"])
 
     def test_multiline_xml(self):
         xml = read_sample_xml("tests/samples/simple.xml")
         evaluated = xpath.evaluate(xml, "//Tag")
 
-        self.assertEqual("Tag", evaluated[0]["display_text"])
+        self.assertEqual("Tag", evaluated[0]["match_text"])
         self.assertEqual(3, evaluated[0]["line_number"])
 
     def test_large_xml_line_number(self):
@@ -38,3 +38,54 @@ class XPathTests(unittest.TestCase):
         self.assertEqual(135532, evaluated[4]["line_number"])
         self.assertEqual(135533, evaluated[5]["line_number"])
         self.assertEqual(139998, evaluated[6]["line_number"])
+
+    def test_namespaces(self):
+        namespaces = dict()
+        namespaces["a"] = "http://someurl.org"
+        namespaces["b"] = "http://anotherurl.org"
+
+        xml = read_sample_xml("tests/samples/namespaces.xml")
+        evaluatedA = xpath.evaluate(xml, "//a:Tag", namespaces)
+        evaluatedB = xpath.evaluate(xml, "//b:Tag", namespaces)
+
+        self.assertEqual(3, evaluatedA[0]["line_number"])
+        self.assertEqual("a:Tag", evaluatedA[0]["match_text"])
+
+        self.assertEqual(4, evaluatedB[0]["line_number"])
+        self.assertEqual("b:Tag", evaluatedB[0]["match_text"])
+
+    def test_node(self):
+        xml = read_sample_xml("tests/samples/simple.xml")
+        evaluated = xpath.evaluate(xml, "//Tag")
+
+        self.assertEqual("Node", evaluated[0]["match_type"])
+        self.assertEqual(3, evaluated[0]["line_number"])
+        self.assertEqual("Tag", evaluated[0]["match_text"])
+        self.assertEqual("", evaluated[0]["value_text"])
+
+    def test_attribute(self):
+        xml = read_sample_xml("tests/samples/simple.xml")
+        evaluated = xpath.evaluate(xml, "//TagWithAttribute/@attribute")
+
+        self.assertEqual("Attribute", evaluated[0]["match_type"])
+        self.assertEqual(5, evaluated[0]["line_number"])
+        self.assertEqual("@attribute", evaluated[0]["match_text"])
+        self.assertEqual("attribute text", evaluated[0]["value_text"])
+
+    def test_text(self):
+        xml = read_sample_xml("tests/samples/simple.xml")
+        evaluated = xpath.evaluate(xml, "//TagWithText/text()")
+
+        self.assertEqual("String", evaluated[0]["match_type"])
+        self.assertEqual(4, evaluated[0]["line_number"])
+        self.assertEqual("", evaluated[0]["match_text"])
+        self.assertEqual("element text", evaluated[0]["value_text"])
+
+    def test_parentless_text(self):
+        xml = read_sample_xml("tests/samples/simple.xml")
+        evaluated = xpath.evaluate(xml, "'hello there'")
+
+        self.assertEqual("String", evaluated[0]["match_type"])
+        self.assertEqual(None, evaluated[0]["line_number"])
+        self.assertEqual("", evaluated[0]["match_text"])
+        self.assertEqual("hello there", evaluated[0]["value_text"])
--- python/vim_xml_tools/xpath.py
+++ python/vim_xml_tools/xpath.py
@@ -1,25 +1,31 @@
+import re
 from lxml import etree
 
 LIBXML2_MAX_LINE = 65534
 
-def evaluate(xml, xpath):
+def evaluate(xml, xpath, namespaces=dict()):
     """Evaluate an xpath against some xml. 
     Reports line numbers correctly on xml with over 65534 lines"""
 
     tree = etree.fromstring(xml)
-    tree_matches = tree.xpath(xpath)
+    tree_matches = tree.xpath(xpath, namespaces=namespaces)
+
+    if not(isinstance(tree_matches, list)):
+        tree_matches = [tree_matches]
 
     matches = []
     line_compress_required = False
 
     for match in tree_matches:
-        if match.sourceline <= LIBXML2_MAX_LINE:
+        output_match = tree_match_to_output_match(match, namespaces)
+
+        if (output_match["line_number"] is None) or \
+                output_match["line_number"] <= LIBXML2_MAX_LINE:
             #Only keep matches which are in the line range that libxml2
             #actually reports correctly
-            output_match = tree_match_to_output_match(match)
             matches.append(output_match)
         else:
-            #If there are any higher range matches, we will have to 'fold'
+            #If there are any higher range matches, we will have to 'compress lines'
             line_compress_required = True
             break;
 
@@ -45,10 +51,89 @@ def evaluate(xml, xpath):
 
     return matches
 
-def tree_match_to_output_match(match):
+def tree_match_to_output_match(match, namespaces):
     """Convert an lxml xpath match to a result output dictionary"""
     out = dict()
-    out["line_number"] = match.sourceline
-    out["display_text"] = match.tag
+    out["line_number"] = output_line_number(match)
+    out["match_type"] = output_match_type(match)
+    out["match_text"] = output_match_text(match, namespaces)
+    out["value_text"] = output_value_text(match)
 
     return out
+
+def output_line_number(match):
+    """Return an output source line for a particular match result"""
+    sourceline = None
+
+    if isinstance(match, etree._Element):
+        sourceline = match.sourceline
+
+    if isinstance(match, etree._ElementStringResult):
+        parent = match.getparent()
+        if parent is not None:
+            sourceline = parent.sourceline
+
+    return sourceline
+
+def output_match_type(match):
+    """Return an output 'type' for a particular match result"""
+    match_type = "?"
+
+    if isinstance(match, etree._Element):
+        match_type = "Node"
+
+    elif isinstance(match, etree._ElementStringResult):
+        if match.is_attribute:
+            match_type = "Attribute"
+        else:
+            match_type = "String"
+
+    return match_type
+
+def output_match_text(match, namespaces):
+    """Text representing the match of the XPath"""
+    match_text = ""
+
+    if isinstance(match, etree._Element):
+        match_text = prefixed_name_from_absolute_name(match.tag, namespaces)
+
+    if isinstance(match, etree._ElementStringResult):
+        if match.is_attribute:
+            match_text = "@" + match.attrname
+
+    if match_text is None:
+        match_text = ""
+
+    return match_text
+
+def output_value_text(match):
+    """Text representing the value of the XPath"""
+    value_text = ""
+
+    if isinstance(match, etree._Element):
+        value_text = match.text
+
+    elif isinstance(match, etree._ElementStringResult):
+        value_text = match
+
+    elif isinstance(match, str):
+        value_text = match
+
+    if value_text is None:
+        value_text = ""
+
+    return value_text
+
+def prefixed_name_from_absolute_name(name, namespaces):
+    """Convert an lxml {namespaceuri}Name to prefix:Name,
+    with prefixes defined in the namespaces dictionary"""
+    regex_match = re.search('^\{(.*)\}', name)
+    if regex_match:
+        uri = regex_match.group(1)
+        uri_with_braces = regex_match.group(0)
+
+        prefix = [k for k, v in namespaces.items() if v == uri][0]
+        prefixed = name.replace(uri_with_braces, "{0}:".format(prefix))
+
+        return prefixed
+    return name
