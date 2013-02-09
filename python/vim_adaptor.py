7e7a0e99493939537aecd1ba8fde1a9962e35152
Dave Aitken <dave.aitken@gmail.com>
2013-02-09 17:43:24 +0000

    Actually call out to xpath evaluator

--- python/vim_adaptor.py
+++ python/vim_adaptor.py
@@ -3,6 +3,8 @@ try:
     import vim
 except ImportError:
     vim = None
+    
+from vim_xml_tools import xpath as x
 
 VARIABLE_SCOPE = "s:"
 
@@ -13,12 +15,22 @@ def evaluate_xpath_on_current_buffer(xpath):
     eval_list = []
     eval_list.append("let {0}xpath_results_list = []".format(VARIABLE_SCOPE))
     
-    eval_list.append(("let {0}result_dict = " + 
-                      "{{bufnr: {1}, lnum: {2}, text: '{3}'}}"
-                     ).format(VARIABLE_SCOPE,
-                              vim.current.buffer.number, 2, '<Tag>'))
-    eval_list.append(("let {0}xpath_results_list += {0}result_dict"
-                     ).format(VARIABLE_SCOPE)) 
+    xml = get_current_buffer_string()
+    for result in x.evaluate(xml, xpath, {}):
+        
+        eval_list.append(("let {0}result_dict = " + 
+                          "{{bufnr: {1}, lnum: {2}, text: '{3}{4}'}}"
+                         ).format(VARIABLE_SCOPE,
+                                  vim.current.buffer.number, 
+                                  result["line_number"], 
+                                  result["match"],
+                                  result["value"]))
+                         
+        eval_list.append(("let {0}xpath_results_list += {0}result_dict"
+                         ).format(VARIABLE_SCOPE)) 
+
+    eval_list.append(("setloclist(0, {0}xpath_results_list, 'r')"
+                     ).format(VARIABLE_SCOPE))
 
     for eval_line in eval_list:
         vim.eval(eval_line)
