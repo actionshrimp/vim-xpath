8c29cd2677c1f4072fb40470aebe33e3a9ad16f0
Dave Aitken <dave.aitken@gmail.com>
2013-02-09 19:08:26 +0000

    Added a general XPathError wrapping exception

--- python/vim_xml_tools/exceptions.py
+++ python/vim_xml_tools/exceptions.py
@@ -1,11 +1,22 @@
 from lxml import etree
 
 def from_lxml_exception(e):
+    out = e
+
     if isinstance(e, etree.XPathError):
+        out = XPathError(e)
+
         if 'XPATH_UNDEF_PREFIX_ERROR' in str(e.error_log):
-            return XPathNamespaceUndefinedError(e)
+            out = XPathNamespaceUndefinedError(e)
+
+    return out
 
 class XPathNamespaceUndefinedError(Exception):
+    def __init__(self, e):
+        self.inner = e
+        self.message = e.message
+
+class XPathError(Exception):
     def __init__(self, e):
         self.inner = e
         self.message = e.message
--- python/vim_xml_tools/tests/exceptions_tests.py
+++ python/vim_xml_tools/tests/exceptions_tests.py
@@ -3,17 +3,29 @@ from lxml import etree
 
 from vim_xml_tools.exceptions import from_lxml_exception
 from vim_xml_tools.exceptions import XPathNamespaceUndefinedError
+from vim_xml_tools.exceptions import XPathError
 
 class ExceptionsTests(unittest.TestCase):
 
+    def setUp(self):
+        self.tree = etree.fromstring("<Root><Tag/></Root>")
+
     def test_undefined_namespace_in_xpath_error_is_converted(self):
-        tree = etree.fromstring("<Root><Tag/></Root>")
         error = None
         try:
-            tree.xpath("//a:Tag")
+            self.tree.xpath("//a:Tag")
         except Exception as e:
             error = e
 
         converted = from_lxml_exception(error)
-
         self.assertIsInstance(converted, XPathNamespaceUndefinedError)
+
+    def test_xpath_syntax_error_is_converted(self):
+        error = None
+        try:
+            self.tree.xpath("//A/Bad/XPa()th")
+        except Exception as e:
+            error = e
+
+        converted = from_lxml_exception(error)
+        self.assertIsInstance(converted, XPathError)
