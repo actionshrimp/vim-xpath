5534e37356c3b6085352d35e6e21b37efa4dcb93
Dave Aitken <dave.aitken@gmail.com>
2013-03-03 13:33:07 +0000

    Add namespace prefix guesser tests with blank module

---
+++ python/vim_xml_tools/tests/namespace_prefix_guesser_tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+from helpers.file_helpers import read_sample_xml
+
+from vim_xml_tools import namespace_prefix_guesser
+
+class NamespacePrefixGuesserTests(unittest.TestCase):
+    pass
